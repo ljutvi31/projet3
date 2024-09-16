1abd098c36f9b542aebc7f06c8a7e9ed90e74052
Harry Ljutvi <ljutvi.h@gmail.com>
2024-09-16 14:13:12 +0200

    script is input in html

--- Backend/app.js
+++ Backend/app.js
@@ -20,6 +20,7 @@ const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
 db.sequelize.sync().then(()=> console.log('db is ready'));
+app.use('/test',(req,res)=>res.send("hello world"))
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
