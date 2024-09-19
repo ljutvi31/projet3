d0c79ef230b4c9f4cacb8b7aef05858049fd6461
Harry Ljutvi <ljutvi.h@gmail.com>
2024-09-19 19:39:28 +0200

    first step etablished

--- Backend/server.js
+++ Backend/server.js
@@ -39,6 +39,7 @@ const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
+	console.log("ok")
 	const address = server.address();
 	const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
 	console.log('Listening on ' + bind);
