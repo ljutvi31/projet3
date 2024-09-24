6c4420c0e1ce4e8a5f1aff8a2aee406422644170
Harry Ljutvi <ljutvi.h@gmail.com>
2024-09-24 19:18:54 +0200

    filter buttons added and event listener on click

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,7 @@
 // Fonction pour récupérer les travaux via une API Utilisation de 'await' pour attendre la réponse de l'API
 async function getApiWorks() {
   try {
-    const response = await fetch("http://localhost:5678/api/works");
-    // Renvoie des données au format JSON
+    const response = await fetch("http://localhost:5678/api/works"); // Renvoie des données au format JSON
     return await response.json();
   } catch (error) {
     // Affichage de l'erreur dans la console en cas de problème
@@ -29,9 +28,40 @@ function createFigureElement(work) {
   return figureElement;
 }
 
-// Fonction principale pour insérer les travaux dans la galerie et  Récupération des travaux via l'API
+function getCategories(works) {
+  // recup des noms de categories sur works
+  const categoryNames = works.map((work) => work.category.name);
+  const uniqueCategoryNames = new Set(categoryNames); // map est comme une boucle et set permet deviter les doublons et de stocker valeurs uniques
+  
+  uniqueCategoryNames.add("Tous");
+  return [...uniqueCategoryNames];
+}
+
+function createFilterButtons(categories) { 
+  const filterContainer = document.createElement("div");
+  filterContainer.classList.add("filter-container");
+  const filterButtons = document.querySelector(".category-menu");
+  categories.forEach((category) => { // fonction flechee selection de chaque catgeorie une à une
+    const button = document.createElement("button");
+    button.innerText = category;
+ filterButtons.appendChild(button);
+ button.addEventListener('click', function(event){
+  onClickFilterButton(category);
+ })
+  });
+}
+
+function onClickFilterButton(category){
+  console.log(category);
+
+}
+
+
+// Fonction principale pour insérer les travaux dans la galerie, récupération des travaux via l'API et des catégories dans travaux
 async function createGalleryElement() {
   const works = await getApiWorks();
+  const categories = getCategories(works);
+  createFilterButtons(categories);
 
   // Sélection de la galerie où insérer les figures
   const galleryHtml = document.querySelector(".gallery");
@@ -45,9 +75,3 @@ async function createGalleryElement() {
 
 // Exécution de la fonction principale
 createGalleryElement();
-
-function getCategories(works) { // récupération catégories par travaux
-  const categories = new Set(works.map((work) => work.category.name)); // utilisation de Set pour chercher toutes les catégories et éliminer les doublons
-  categories.add("Par defaut"); // ajout de la catégorie par defaut
-  return [...categories]; // catégories sous forme d'un tableau
-}
